d0ec07f3cf7b5c879e70fa536defbc6c76427a35
Unknown <greezlie@gmail.com>
2017-11-01 11:55:58 +0100

    Create c++ project

---
+++ cmake-build-debug/CMakeFiles/ZMPO_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZMPO_3.dir/main.cpp.o"
+  "ZMPO_3.pdb"
+  "ZMPO_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZMPO_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
